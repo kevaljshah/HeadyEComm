abe057c3598eae80b50ad3f334b3edab630029ff
Keval Shah <keval.shah1411@gmail.com>
2020-04-06 17:49:26 +0530

    updates

--- HeadyEComm/ViewControllers/ProductsListViewController.swift
+++ HeadyEComm/ViewControllers/ProductsListViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-private let reuseIdentifier = "Cell"
+private let reuseIdentifier = "productsListCell"
 
 class ProductsListViewController: UICollectionViewController {
     
@@ -16,20 +16,20 @@ class ProductsListViewController: UICollectionViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        print(category)
-        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
     }
 
-    /*
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "showProduct" {
+            if let destinationNavigationController = segue.destination as? UINavigationController,
+            let targetController = destinationNavigationController.topViewController as? ProductViewController,
+                let productIndex = collectionView.indexPathsForSelectedItems?.first?.row
+            {
+                targetController.product = category.products[productIndex]
+            }
+        }
     }
-    */
 
     // MARK: UICollectionViewDataSource
 
@@ -45,10 +45,12 @@ class ProductsListViewController: UICollectionViewController {
     }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
-    
-        // Configure the cell
-    
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! ProductsListCollectionViewCell
+        cell.configure(with: category.products[indexPath.row])
         return cell
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "showProduct", sender: self)
     }
 }
